cfdc2747b2b951437839ce200ee05b285355a86e
Seanlangbrown <seanlangbrown@gmail.com>
2017-12-19 01:21:02 -0800

    implemented _.reduce()

--- src/underbar.js
+++ src/underbar.js
@@ -102,7 +102,7 @@
   _.uniq = function(array, isSorted, iterator) {
     var results = [];
     if(isSorted){
-      //faster implementation, optional iterator function
+      //sorting allows faster implementation, optional iterator function
       
       function transform(val) {
         var t = (iterator === undefined) ? _.identity : iterator;
@@ -124,7 +124,7 @@
 
     }
     return results;
-    
+
   };
 
 
@@ -133,6 +133,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var results = [];
+    _.each(collection, function(value) {
+      results.push(iterator.apply(null, [value]));
+    })
+    return results;
   };
 
   /*
@@ -174,6 +179,20 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+
+    var result;
+    var workingCollection = collection.slice();
+    if(accumulator !== undefined) {
+      result = accumulator;
+    } else {
+      result = workingCollection.shift();
+    }
+
+    _.each(workingCollection, function(value) {
+      result = iterator.apply(null, [result, value]);
+      });
+    
+    return result;
   };
 
   
